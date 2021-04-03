489ff1b32d4840c20ed106b8943e07b367449b70
⎝◣⏝◢⎠SANTRI-CYBER-SANTUY⎝◣⏝◢⎠ <79558956+AKKUSANTUY@users.noreply.github.com>
2021-04-03 10:48:05 +0700

    Update and rename spam.py to Kuota.sh

---
+++ Kuota.sh
@@ -0,0 +1,75 @@
+#usr/bin/bash
+clear
+bi='\33[34;1m' #biru
+ij='\33[32;1m' #ijo
+pr='\33[35;1m' #purple
+cy='\33[36;1m' #cyan
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+echo
+figlet -f slant  "Paket"|lolcat
+
+figlet -f slant "       kuota"|lolcat
+echo $ku"        ┕──━──━──┑◆┍──━──━──┙"
+echo $ku"        🇵 🇦 🇰 🇪 🇹  🇬 🇷 🇦 🇹 🇮 🇸"
+echo $bi"╔╦╗╔╦╗╔═╗╔══╗╔══╗  ╔══╗╔═╗╔══╗╔══╗╔══╗╔══╗"
+echo $bi"║╔╝║║║║║║╚╗╔╝║╔╗║  ║╔═╣║╬║║╔╗║╚╗╔╝╚║║╝║══╣"
+echo $bi"║╚╗║║║║║║─║║─║╠╣║  ║╚╗║║╗╣║╠╣║─║║─╔║║╗╠══║"
+echo $bi"╚╩╝╚═╝╚═╝─╚╝─╚╝╚╝  ╚══╝╚╩╝╚╝╚╝─╚╝─╚══╝╚══╝"
+echo $ku"─────────────────  ───────────────────────"
+echo $ij"╚─━━━░★░━━━─╝╚─━━━░★░━━━─╝╚─━━━━░★░━━━━━━─╝"
+echo
+echo $ij"║▌│█▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│*"
+echo
+echo $pu"▂ ▃ ▄ ▅ ▆ ▇ █ █4G" $me"Telkomsel"
+echo
+echo
+echo $ku"gunakan No :(63xxxxx)"
+read -p "|___--->✓ : pilih : " pil
+echo "L SEDANG MEMPROSES"
+sleep 1
+echo "LOADING.......10%"
+sleep 1
+echo "LOADING.......20%"
+sleep 1
+echo "LOADING.......30%"
+sleep 1
+echo "LOADING.......40%"
+sleep 1
+echo "LOADING.......50%"
+sleep 1
+echo "LOADING.......60%"
+sleep 1
+echo "LOADING.......70%"
+sleep 1
+echo "LOADING.......80%"
+sleep 1
+echo "LOADING.......90%"
+sleep 1
+echo "LOADING.......100%"
+sleep 1
+echo "\033[32;1mSilahkan Masukkan Jumlah Yang Anda Mau:"
+read p;
+echo
+echo "Sedang Menuju Proses..."
+sleep 1
+echo "■□□□□20%"
+sleep 3
+echo "■■□□□40%"
+sleep 3
+echo "■■■□□60%"
+sleep 3
+echo "■■■■□80%"
+sleep 3
+echo "■■■■□90%"
+sleep 4
+echo "■■■■■100%"
+sleep 3
+echo "██████████████]100% berhasil scan"
+echo "\033[34;1mjumlah yang anda minta sedang kosong ,coba pilih nomor lain...""\033[32;1msemoga di lain waktu anda beruntung"
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+echo "TerimaKasih Untuk Yang Memakai Script Ini"
+exit
